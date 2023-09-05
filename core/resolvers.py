f4e719e61279c80d0e658b2504c334ecf4cb6527
Carmen <pr.carmengalvan@gmail.com>
2023-09-05 20:13:11 +0200

    feat: create idea

--- core/resolvers.py
+++ core/resolvers.py
@@ -1,6 +1,9 @@
 from users.models import CustomUser
 from django.contrib.auth import get_user_model
 from gqlauth.models import UserStatus
+from ideas.models import Idea
+from gqlauth.core.utils import get_user
+from strawberry.types import Info
 
 def register_user(username: str, email: str, password: str) -> CustomUser:
     CustomUser = get_user_model()
@@ -11,3 +14,8 @@ def register_user(username: str, email: str, password: str) -> CustomUser:
     status.save()
 
     return user
+
+def create_idea(text: str, visibility: str, info: Info) -> Idea:
+    user = get_user(info)
+    idea = Idea.objects.create(text = text, visibility = visibility, user = user)
+    return idea
