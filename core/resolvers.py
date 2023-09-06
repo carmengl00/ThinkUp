2d4fdbde8d8e2c4f559d52305da1e1753ffef6ad
Carmen <pr.carmengalvan@gmail.com>
2023-09-06 16:56:03 +0200

    feat: search user

--- core/resolvers.py
+++ core/resolvers.py
@@ -95,3 +95,9 @@ def delete_follower(username: str, info: Info) -> bool:
     else:
         raise ValueError('This user is not following you')
     return True
+
+# A user can search other user by username or part of it
+def search_user(username: str, info: Info) -> List[CustomUser]:
+    user = get_user(info)
+    return get_user_model().objects.filter(username__icontains = username).exclude(username = user.username)
+    
