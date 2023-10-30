3fc13e0b18dba8e4e8cb71a7d238e099407e9933
Carmen <carmengalvan@z1.digital>
2023-10-30 12:55:24 +0100

    fix: auxiliar method return List[IdeaType], not PaginatedIdeaType

--- ideas/utils.py
+++ ideas/utils.py
@@ -1,12 +1,10 @@
 from typing import List
-from base.graphql.inputs import PaginationInput
-from base.graphql.utils import get_paginator
-from ideas.graphql.types import IdeaType, PaginatedIdeaType
+from ideas.graphql.types import IdeaType
 from ideas.models import Idea
 from users.models import CustomUser, Follows
 from ideas.graphql.types import VisibilityEnum
 
-def ideas_user_aux(user_authenticated: CustomUser, target_user: CustomUser) -> PaginatedIdeaType:
+def ideas_user_aux(user_authenticated: CustomUser, target_user: CustomUser) -> List[IdeaType]:
     lista = []
     if user_authenticated == target_user:
         lista = Idea.objects.filter(user = target_user)
