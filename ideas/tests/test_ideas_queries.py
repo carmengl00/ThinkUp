31beec42d728a9fd15abbf6922747bef4a66f67b
Carmen <carmengalvan@z1.digital>
2023-11-13 19:07:58 +0100

    test: refactor notification query ordering by username and tests

--- ideas/tests/test_ideas_queries.py
+++ ideas/tests/test_ideas_queries.py
@@ -4,39 +4,199 @@ import pytest
 from mixer.backend.django import mixer
 from base.factory_test_case import TestBase
 from unittest.mock import patch
-from ideas.models import Idea
+from ideas.models import Idea, Notification, VisibilityType
+from users.models import CustomUser, Follows
 
 IDEAS_ITEMS = """
-    pageInfo {
-        page
-        pageSize
-        totalResults
-    }
-    edges {
-        node {
-            id
-            title
-            description
-            user {
-                uuid
+    query($pagination: PaginationInput!) {
+        myIdeas(pagination: $pagination) {
+            pageInfo {
+                page
+			    pages
+            }
+            edges {
+                id
+                text
+                createdAt
+                updatedAt
+                visibility
+                user{
+                    username
+                }
             }
         }
-    }"""
+    }
+"""
+IDEAS_USER = """
+    query($id: UUID!, $pagination: PaginationInput!) {
+        ideasUser(id: $id, pagination: $pagination) {
+            pageInfo {
+                page
+                pages
+            }
+            edges {
+                id
+                text
+                createdAt
+                updatedAt
+                visibility
+                user{
+                    username
+                }
+            }
+        }
+    }
+"""
+TIMELINE = """
+    query($pagination: PaginationInput!) {
+        timeline(pagination: $pagination) {
+            pageInfo {
+                page
+			    pages
+            }
+            edges {
+                id
+                text
+                createdAt
+                updatedAt
+                visibility
+                user{
+                    username
+                }
+            }
+        }
+    }
+"""
 
+NOTIFICATIONS = """
+    query($pagination: PaginationInput!) {
+        myNotifications(pagination: $pagination) {
+            pageInfo {
+                page
+                pages
+            }
+            edges {
+                id
+                idea{
+                    text
+                }
+                user{
+                    username
+                }
+            }
+        }
+    }
+"""
 
 @pytest.mark.django_db
 class TestIdeasQueries(TestBase):
-    @patch("gqlauth.core.middlewares.django_jwt_middleware")
-    def test_my_ideas(self, mock):
-        mock.return_value=lambda get_response: get_response
-        mixer.cycle(5).blend(Idea, user=self.user)
+    def test_my_ideas(self):
+        mixer.blend(Idea, user=self.user, text="idea 1")
+        mixer.blend(Idea, user=self.user, text="idea 2")
+        mixer.blend(Idea, user=self.user, text="idea 3")
+
+        variables = {
+            "pagination": {
+                "page": 1,
+                "pageSize": 5,
+            },
+        }
 
         response = self.post(
             query=IDEAS_ITEMS,
             user=self.user,
-            variables={"input": {"page": 1, "pageSize": 5}}
+            variables= variables
         )
 
         data = json.loads(response.content.decode())
-        assert len(data["data"]["myIdeas"]["edges"]) == 5
-        assert data["data"]["myIdeas"]["pageInfo"]["totalResults"] == 5
+        ideas = data.get("data").get("myIdeas").get("edges")
+        assert len(ideas) == 3
+
+        assert ideas[0].get("text") == "idea 3"
+        assert ideas[1].get("text") == "idea 2"
+        assert ideas[2].get("text") == "idea 1"
+
+    def test_ideas_user(self):
+        user2 = mixer.blend(CustomUser, username = "user2")
+        mixer.blend(Idea, user=user2, text="idea 1")
+        mixer.blend(Idea, user=user2, text="idea 2")
+        mixer.blend(Idea, user=user2, text="idea 3")
+
+        variables = {
+            "id": str(user2.id),
+            "pagination": {
+                "page": 1,
+                "pageSize": 5,
+            },
+        }
+
+        response = self.post(
+            query=IDEAS_USER,
+            user=self.user,
+            variables= variables
+        )
+
+        data = json.loads(response.content.decode())
+        ideas = data.get("data").get("ideasUser").get("edges")
+        assert len(ideas) == 3
+
+        assert ideas[0].get("text") == "idea 3"
+        assert ideas[1].get("text") == "idea 2"
+        assert ideas[2].get("text") == "idea 1"
+
+    def test_timeline(self):
+        user2 = mixer.blend(CustomUser, username = "user2")
+        mixer.blend(Idea, user=user2, text="idea 1", visibility = VisibilityType.PUBLIC)
+        mixer.blend(Idea, user=user2, text="idea 2", visibility = VisibilityType.PROTECTED)
+        mixer.blend(Idea, user=user2, text="idea 3", visibility = VisibilityType.PRIVATE)
+        mixer.blend(Idea, user=self.user, text="idea 4")
+        mixer.blend(Idea, user=self.user, text="idea 5")
+
+        mixer.blend(Follows, follower=self.user, followed=user2)
+
+        variables = {
+            "pagination": {
+                "page": 1,
+                "pageSize": 5,
+            },
+        }
+
+        response = self.post(
+            query=TIMELINE,
+            user=self.user,
+            variables= variables
+        )
+
+        data = json.loads(response.content.decode())
+        ideas = data.get("data").get("timeline").get("edges")
+        assert len(ideas) == 4
+
+        assert ideas[0].get("text") == "idea 5"
+        assert ideas[1].get("text") == "idea 4"
+        assert ideas[2].get("text") == "idea 2"
+        assert ideas[3].get("text") == "idea 1"
+
+    def test_my_notifications(self):
+        mixer.blend(Notification, user=self.user, idea=mixer.blend(Idea, user=self.user, text="idea 1"))
+        mixer.blend(Notification, user=self.user, idea=mixer.blend(Idea, user=self.user, text="idea 2"))
+
+        variables = {
+            "pagination": {
+                "page": 1,
+                "pageSize": 5,
+            },
+        }
+
+        response = self.post(
+            query=NOTIFICATIONS,
+            user=self.user,
+            variables= variables
+        )
+
+        data = json.loads(response.content.decode())
+        print(data)
+        ideas = data.get("data").get("myNotifications").get("edges")
+        assert len(ideas) == 2
+
+        assert ideas[0].get("idea").get("text") == "idea 1"
+        assert ideas[1].get("idea").get("text") == "idea 2"
