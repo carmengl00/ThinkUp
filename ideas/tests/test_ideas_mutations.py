fb4f2343c5dcdb3d2b419fbdc5c41f355db2a799
Carmen <carmengalvan@z1.digital>
2023-11-14 11:26:30 +0100

    test: add negative test idea mutations

--- ideas/tests/test_ideas_mutations.py
+++ ideas/tests/test_ideas_mutations.py
@@ -63,6 +63,25 @@ class TestIdeasMutations(TestBase):
         assert len(idea) == 1
         assert idea.get("createIdea").get("user").get("username") == user.username
 
+    def test_create_idea_without_text(self):
+        variables = {
+            "visibility": "PUBLIC",
+        }
+        request = self.request_factory.post(
+            "/graphql/",
+            {
+                "query": CREATE_IDEA,
+                "variables": variables,
+            },
+            content_type="application/json",
+        )
+        request.user = self.user
+        response = GraphQLView.as_view(schema=schema)(request)
+        data = json.loads(response.content.decode())
+        print(data)
+        idea = data.get("data")
+        assert idea == None
+
     def test_update_visibility_idea(self):
         idea = mixer.blend(Idea, user = self.user, text = "idea 1", visibility = VisibilityType.PRIVATE)
         variables = {
@@ -87,6 +106,27 @@ class TestIdeasMutations(TestBase):
         user = CustomUser.objects.get(id=self.user.id)
         assert idea.get("updateVisibilityIdea").get("user").get("username") == user.username
 
+    def test_update_incorrect_visibility_idea(self):
+        idea = mixer.blend(Idea, user = self.user, text = "idea 1", visibility = VisibilityType.PRIVATE)
+        variables = {
+            "id": str(idea.id),
+            "visibility": "INCORRECT",
+        }
+        request = self.request_factory.post(
+            "/graphql/",
+            {
+                "query": UPDATE_VISIBILITY_IDEA,
+                "variables": variables,
+            },
+            content_type="application/json",
+        )
+
+        request.user = self.user
+        response = GraphQLView.as_view(schema=schema)(request)
+        data = json.loads(response.content.decode())
+        idea = data.get("data")
+        assert idea == None
+
     def test_delete_idea(self):
         idea = mixer.blend(Idea, user = self.user, text = "idea 1", visibility = VisibilityType.PRIVATE)
         variables = {
@@ -106,3 +146,22 @@ class TestIdeasMutations(TestBase):
         data = json.loads(response.content.decode())
         idea = data.get("data").get("deleteIdea")
         assert idea == True
+
+    def test_delete_idea_unauthenticated(self):
+        idea = mixer.blend(Idea, user = self.user, text = "idea 1", visibility = VisibilityType.PRIVATE)
+        variables = {
+            "id": str(idea.id),
+        }
+        request = self.request_factory.post(
+            "/graphql/",
+            {
+                "query": DELETE_IDEA,
+                "variables": variables,
+            },
+            content_type="application/json",
+        )
+
+        response = GraphQLView.as_view(schema=schema)(request)
+        data = json.loads(response.content.decode())
+        idea = data.get("data")
+        assert idea == None
